c444a388add56f4fe11207a4619d2c5f2268a7b6
Hunter Blakely <blakelyh@sou.edu>
2013-08-09 16:18:18 -0700

    update
    added source

---
+++ SOURCE/DrgDrpFileCnvrtBinary/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DrgDrpFileCnvrtBinary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clearAll);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(530, 147);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "All";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(45, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Selected";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(129, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Output Save Location";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(129, 201);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.radioButton1);
+            this.splitContainer1.Panel2.Controls.Add(this.radioButton2);
+            this.splitContainer1.Size = new System.Drawing.Size(241, 23);
+            this.splitContainer1.SplitterDistance = 111;
+            this.splitContainer1.TabIndex = 10;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(63, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(45, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Hex";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Binary";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Output Options";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(522, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(20, 20);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Drag files here";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 238);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convert To Binary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SOURCE/DrgDrpFileCnvrtBinary/Form1.cs
@@ -0,0 +1,177 @@
+﻿// Author: Hunter J. Blakely
+// DrgDrpFileCnvrtBinary Copyright 7/14/2013 @5:22pm 
+// Enjoy...
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DrgDrpFileCnvrtBinary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } // end Form1
+
+
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            String[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string file in files)
+                if (listBox1.Items.Contains(file) == false)
+                    listBox1.Items.Add(file);
+        } // end listBox1_DragDrop
+
+
+
+        private void clearAll(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                listBox1.Items.Clear();
+            if (radioButton2.Checked)
+                for (int i = listBox1.SelectedIndices.Count - 1; i >= 0; i--)
+                {
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndices[i]);
+                } // fi
+        } // end clearAll
+
+
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        } // end listBox1_DragEnter
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string outputFolder = textBox1.Text;
+            try { Directory.CreateDirectory(outputFolder); }
+            catch (Exception ex) { Console.WriteLine("\nMessage ---\n{0}", ex.Message); }
+
+            if (radioButton1.Checked)
+                procAll(outputFolder);      
+            else if (radioButton2.Checked)
+                procSelect(outputFolder);
+        } // end button2_Click
+
+
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        } // end textBox1_DragEnter
+
+
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            String file = (string)e.Data.GetData(DataFormats.FileDrop, false);
+            textBox1.Text = file;
+        } // end textBox1_DragDrop
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        } // end textBox1_TextChanged
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string[] split = user.Split('\\');
+            user = split[split.Length - 1];
+            AcceptButton = button2;
+            radioButton1.Checked = true;
+            checkBox1.Checked = true;
+            textBox1.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"\\test";
+        } // end Form1_Load
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        } // end button3_Click
+
+
+
+        private void procAll(string folder)
+        {
+            ListBox.ObjectCollection a = listBox1.Items;
+            try
+            {
+                foreach (string path in a)
+                {
+                    write(folder, path);
+                } // end foreach
+            }
+            catch (Exception ex) { Console.WriteLine("\nMessage ---\n{0}", ex.Message); }
+        } // end procAll
+
+        
+
+        private void procSelect(string folder)
+        {
+            ListBox.SelectedObjectCollection b = listBox1.SelectedItems;
+            try
+            {
+                foreach (string path in b)
+                {
+                    write(folder, path);
+                }// end for
+            }
+            catch (Exception ex) { Console.WriteLine("\nMessage ---\n{0}", ex.Message); }
+        } //end procSelect
+
+
+
+
+        public void write(string folder, string path)
+        {
+            string oFile = Path.Combine(folder, Path.GetFileName(path));
+            string bN = "";         // binary output
+            string hN = "";         // hexedecimal output
+            if (checkBox1.Checked)
+            {
+                bN = oFile + ".binary";
+                if (!File.Exists(bN))
+                {
+                    byte[] fileBytes = File.ReadAllBytes(path);
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (byte b in fileBytes)
+                    {
+                        sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                    }
+                    File.WriteAllText(bN, sb.ToString());
+                }// fi
+            }// fi
+            if (checkBox2.Checked)
+            {
+                hN = oFile + ".hexadecimal";
+                if (!File.Exists(hN))
+                {
+                    byte[] fileBytes = File.ReadAllBytes(path);
+                    string hex = BitConverter.ToString(fileBytes);
+                    hex = hex.Replace("-", "");
+                    File.WriteAllText(hN, hex.ToString());
+                }// fi
+            }// fi
+        } // end write
+
+
+    }// End Class Form1
+} // End Namespace
